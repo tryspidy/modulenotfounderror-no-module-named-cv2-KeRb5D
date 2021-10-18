fe944717a27fd70097266b790d08d25d01ec16ec
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-18 22:53:34 +0530

    Initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+To solve this run the following
+# main opencv
+pip install opencv-python
+# contrib package for the extra features
+pip install opencv-contrib-python
+
+The official installation instructions are on the opencv website.
+More info can be found here: 
+https://www.pyimagesearch.com/opencv-tutorials-resources-guides/
